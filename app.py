a2f21a380a10ce049bef2286090974a5d8dfcab4
seoyeonDev <skylarlee1003@gmail.com>
2026-08-24 20:50:10 +0900

    app.py 오류 수정

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from api.login import *
 from api.card import *
 from api.review import *
 
 
-app = Flask(___name___)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is home!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
