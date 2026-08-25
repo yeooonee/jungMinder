99b70a155b99d491e4b8aa3dff6cfd71460c1564
yeooonee <skylarlee1003@gmail.com>
2026-08-25 17:41:14 +0900

    studylogs create

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 from api.login import *
-from api.card import *
+from api.studylog import *
 from api.review import *
 
+from pymongo import MongoClient
+
+client = MongoClient('localhost',27017)
+db = client.dbjungminder
 
 app = Flask(__name__)
 
@@ -10,5 +14,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# api/*.py 등록
+app.register_blueprint(studylogs_bp)
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
+    
+    
