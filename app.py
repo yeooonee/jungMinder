6dbc3e83dd67490aa4b2a071b5ddc6d58e796806
Hannuri Song <cantornuri0516@gmail.com>
2026-08-26 13:49:25 +0900

    로그인 api 등록, 로그인 플라스크 세션용 비밀번호 암호화

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ app = Flask(__name__)
 # .env 내용 불러오기
 app.config.from_object(Config)
 
+#flask session용 비밀번호 암호화
+app.secret_key = "test-secret-key"
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 # api/*.py 등록
+app.register_blueprint(login_bp)
 app.register_blueprint(studylogs_bp)
 app.register_blueprint(file_bp)
-
 
 
 
