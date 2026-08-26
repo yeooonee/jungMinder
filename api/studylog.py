a9ab19a68205dd7472f4ccdaee1b68881c83492d
Yehoon <aostlxl05@gmail.com>
2026-08-26 23:27:48 +0900

    Index

--- api/studylog.py
+++ api/studylog.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, Flask, render_template, request, jsonify
-from pymongo import MongoClient
-from ref.database import db
+from flask import Blueprint, render_template, request, jsonify, session
 from datetime import datetime
+from ref.database import db
+from bson.objectid import ObjectId
+import os
 
-# app.py 와 연결
+# app.py와 연결되는 블루프린트 설정 (url_prefix가 /studylogs 임)
 studylogs_bp = Blueprint('studylogs', __name__, url_prefix='/studylogs')
 
-# 1. 이미지를 받아 static/uploads 폴더에 저장하고 파일명을 리턴하는 라우터
+# 1. 새 글쓰기 페이지 렌더링 (GET /studylogs/create)
+@studylogs_bp.route('/create', methods=['GET'])
+def studylogs_create_page():
+    # 세션에서 로그인된 유저 이름을 가져와서 템플릿에 전달 (헤더 이름 표시용)
+    user_name = session.get('user_name', '사용자')
+    return render_template('studylogcreate.html', user_name=user_name, studylog=None)
+
+# 2. 이미지를 받아 static/uploads 폴더에 저장하고 파일명을 리턴하는 라우터 (POST /studylogs/upload)
 @studylogs_bp.route('/upload', methods=['POST'])
 def studylogs_image_upload():
     if 'files' not in request.files:
@@ -34,14 +42,14 @@ def studylogs_image_upload():
         'filename': filename
     })
 
-# studylog 생성
+# 3. studylog 생성 라우터 (POST /studylogs/create)
 @studylogs_bp.route('/create', methods=['POST'])
 def studylogs_create():
-    title = request.form['title']
-    content = request.form['content']
-    reg_id = request.form['regId']
-    reg_dt = request.form['regDt']
-    mod_dt = request.form['modDt']
+    title = request.form.get('title')
+    content = request.form.get('content')
+    
+    # 세션에서 현재 로그인된 유저 아이디 가져오기 (없으면 'anonymous')
+    reg_id = session.get('user_id', request.form.get('regId', 'anonymous'))
     
     studylogs = {
         'title' : title,
@@ -58,6 +66,37 @@ def studylogs_create():
     db.studylogs.insert_one(studylogs)
     
     return jsonify({
-    'result': 'success', 
-    'msg': '학습일지가 성공적으로 저장되었습니다!'
-})
+        'result': 'success', 
+        'msg': '학습일지가 성공적으로 저장되었습니다!'
+    })
+
+# 4. 하단 카드 리스트 노출 및 페이징/검색 라우터 (GET /studylogs/list)
+@studylogs_bp.route('/list', methods=['GET'])
+def studylog_list():
+    page = int(request.args.get('page', 1))
+    searchword = request.args.get('searchword', '').strip()
+    items_per_page = 2  # 페이지당 보여줄 카드 개수 (2장)
+    
+    # title 또는 content 둘 다 조회 가능한 검색 쿼리 구성
+    query = {}
+    if searchword:
+        query = {
+            "$or": [
+                {"title": {"$regex": searchword, "$options": "i"}},
+                {"content": {"$regex": searchword, "$options": "i"}}
+            ]
+        }
+    
+    total_count = db.studylogs.count_documents(query)
+    skip_count = (page - 1) * items_per_page
+    
+    studylogs = list(db.studylogs.find(query).skip(skip_count).limit(items_per_page))
+    for n in studylogs:
+        n['_id'] = str(n['_id'])
+        
+    return jsonify({
+        'result': 'success', 
+        'studylogs': studylogs,
+        'total_count': total_count,
+        'current_page': page
+    })
