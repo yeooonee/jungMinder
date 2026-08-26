cdc90da4266bad2c0d03ae4fb58be70b28b74c9b
Hannuri Song <cantornuri0516@gmail.com>
2026-08-26 15:33:57 +0900

    logRUD/조회코드 수정

--- api/studylog.py
+++ api/studylog.py
@@ -2,6 +2,9 @@ from flask import Blueprint, Flask, render_template, request, jsonify, session
 from pymongo import MongoClient
 from ref.database import db
 from datetime import datetime
+# 문자열을 MongoDB의 ObjectId로 변환
+from bson import ObjectId
+import os
 
 # app.py 와 연결
 studylogs_bp = Blueprint('studylogs', __name__, url_prefix='/studylogs')
@@ -63,17 +66,20 @@ def studylogs_create():
 })
 
 # studylog 조회
-@studylogs_bp.route('/read', methods=['GET'])
-def studylogs_read():
+@studylogs_bp.route('/view/<id>', methods=['GET'])
+def studylogs_view(id):
     user_id = session['user_id']
-    studylogs = list(
-        db.studylogs.find({'reg_id': user_id})
-    )
 
-    for studylog in studylogs:
-        studylog['_id'] = str(studylog['_id'])
-
-    return jsonify({
-        'result': 'success';
-        'studylogs': studylogs
+    studylog = db.studylogs.find_one({
+        '_id': ObjectId(id),
+        'reg_id': user_id
     })
+
+    if studylog is None:
+        return '학습일지를 찾을 수 없습니다.', 404
+
+    #ssr
+    return render_template(
+        'studylog_view.html',
+        studylog=studylog
+    )
